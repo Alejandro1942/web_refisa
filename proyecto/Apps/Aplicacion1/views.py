bbae2c4b394d0eacb55540f5c535fca2e605677c
Alejandro Garcia <overdrive1942@gmail.com>
2024-10-09 15:06:56 -0600

    Se agregaron imagenes para el inicio slide y se crearon archivos html para cada aprtado en la sección

--- proyecto/Apps/Aplicacion1/views.py
+++ proyecto/Apps/Aplicacion1/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'Aplicacion1/index.html')
+
+def productos(request):
+    return render(request, 'Aplicacion1/productos.html')

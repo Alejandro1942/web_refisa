f5372bd1e0aadd22b8b4e3ea69094b412c422bcb
Alejandro García <overdrive1942@gmail.com>
2024-11-05 15:32:37 -0600

    Se presenta avnace para poder hacer un despliegue de prueba.

--- proyecto/Apps/Aplicacion1/urls.py
+++ proyecto/Apps/Aplicacion1/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('productos/', views.productos, name='productos')
+    path('productos/', views.productos, name='productos'),
+    path('servicios/', views.servicios, name='servicios'),
+    path('compramos/', views.compramos, name='compramos'),
+    path('contacto/', views.contacto, name='contacto'),
 ]
--- proyecto/Apps/Aplicacion1/views.py
+++ proyecto/Apps/Aplicacion1/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'Aplicacion1/index.html')
 
 def productos(request):
     return render(request, 'Aplicacion1/productos.html')
+
+def servicios(request):
+    return render(request, 'Aplicacion1/servicios.html')
+
+def compramos(request):
+    return render(request, 'Aplicacion1/compramos.html')
+
+def contacto(request):
+    return render(request, 'Aplicacion1/contacto.html')
